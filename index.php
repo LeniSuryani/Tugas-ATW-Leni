b8aebc04e2a58027ed8cfd78057f25ea70eba305
LeniSuryani <71055779+LeniSuryani@users.noreply.github.com>
2020-09-10 21:52:16 +0700

    episode 2
    membahas mengenai class dan method

---
+++ index.php
@@ -0,0 +1,38 @@
+<!-- episode 2 mengenai class dan method -->
+
+
+<!-- Product merupakan class -->
+<!-- merek, tipe dll merupakan atribut, atribut di gunakan untuk inisialisasai/prepare -->
+<!-- method adalah function pada php ini -->
+<!-- function jika di PBO -->
+<!-- jika di pbo class adalah blueprint/maket/prototype dari objek yg akan di buat
+
+atribut adalah: sesuatu yg melekat kedalam class tersebut
+
+function adalah: sebuah kelakukan yang bisa dilakukan oleh class tsb
+
+ -->
+
+<? php 
+
+class Mobil {
+    public $merek, $tipe, $mesin, $max_speed;
+
+    // pada Variabel scope harus menggunakan $this untuk memanggil variabel luar
+    public function cetakTipe(){
+        return $this->tipe;
+    }
+
+    function kecepatanMaksimal(){
+        return "Kecepatan maksimal dari mobil ini adalah : ".$this->max_speed;
+    }
+}
+
+$bmw = new Mobil;
+$bmw->merek = "BMW";
+$bmw->tipe = "320i";
+$bmw->mesin = "2000cc";
+$bmw->max_speed = "280km/h";
+
+echo "Tipe ".$bmw->cetakTipe();
+echo "Tipe BMW mempunyai: ".$bmw->kecepatanMaksimal();
