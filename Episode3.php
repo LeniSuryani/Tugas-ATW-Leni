aed14d6d6288009c9e47ca961afed56e4b023005
LeniSuryani <71055779+LeniSuryani@users.noreply.github.com>
2020-09-12 14:09:58 +0700

    Menambahkan komentar di tugas minggu 1

--- Episode3.php
+++ Episode3.php
@@ -10,42 +10,19 @@ setiap object tidak akan ada yg sama persis
 
 class Mobil
 {
-    public $merek, $tipe, $mesin, $max_speed;
-
-    // pada Variabel scope harus menggunakan $this untuk memanggil variabel luar
-    public function cetakTipe()
-    {
-        return $this->tipe;
-    }
-
-    function kecepatanMaksimal()
-    {
-        return "Kecepatan maksimal dari mobil ini adalah : " . $this->max_speed;
-    }
+    public $merek;
 }
-
 $bmw = new Mobil;
-$bmw->merek = "BMW";
 $bmw->tipe = "320i";
-$bmw->mesin = "2000cc";
-$bmw->max_speed = "280km/h";
-
 $bmw2 = new Mobil;
-$bmw2->merek = "BMW";
 $bmw2->tipe = "320i";
-$bmw2->mesin = "2000cc";
-$bmw2->max_speed = "280km/h";
 
-
-echo "$bmw2->tipe";
-echo "<br>";
+echo "$bmw2->tipe  <br>";
 echo "$bmw->tipe";
-
-// untuk membuktikan bahwa setiap object itu berbeda walau pun mempunyai isi yg sama
-// dapat di buktikan dengan berikut
 
 if ($bmw === $bmw2) {
     echo "sama";
 } else {
     echo "   tidak";
 }
+?>
--- Tugas-minggu-1.php
+++ Tugas-minggu-1.php
@@ -1,14 +1,38 @@
 <?php
 
-class Binatang
+// Class
+class Hewan
 {
-    public $nama, $jenis, $berkaki;
+    // Property
+    public  $jumlahKaki,
+        $bisa_terbang = "Tidak Bisa Terbang",
+        $bersuara;
 }
 
-class Kucing extends Binatang
+// inherintance class child
+class Kucing extends Hewan
 {
-    public $bisa_terbang = "Tidak bisa terbang";
 
+    // method
+    function jumlahKaki()
+    {
+        return $this->jumlahKaki;
+    }
+    function terbang()
+    {
+        return $this->bisa_terbang;
+    }
+    function bersuara()
+    {
+        return "meong";
+    }
+}
+
+// inherintance Class Child
+class Anjing extends Hewan
+{
+
+    // method
     function jumlahKaki()
     {
         return $this->jumlahKaki;
@@ -19,16 +43,92 @@ class Kucing extends Binatang
     }
     public function bersuara()
     {
-        return "meong";
+        return "Guk Guk";
+    }
+}
+
+// inherintance Class Child
+class Elang extends Hewan
+{
+    // Property
+    public $bisa_terbang = "Bisa Terbang";
+
+    // method
+    function jumlahKaki()
+    {
+        return $this->jumlahKaki;
+    }
+    public function terbang()
+    {
+        return $this->bisa_terbang;
+    }
+    public function bersuara()
+    {
+        return "Miiip";
+    }
+}
+
+// inherintance Class Child
+class Angsa extends Hewan
+{
+    // Property
+    public $bisa_terbang = "Bisa Terbang";
+
+    // method
+    function jumlahKaki()
+    {
+        return $this->jumlahKaki;
+    }
+    public function terbang()
+    {
+        return $this->bisa_terbang;
+    }
+    public function bersuara()
+    {
+        return "Kwaaak";
     }
 }
 
 
+
+
+
+echo "<hr><hr><hr>";
 $momo = new kucing;
 $momo->jumlah_kaki = 4;
 echo "momo adalah kucing <br>";
-echo "punya kaki sebanyak : " . $momo->jumlah_kaki . "<br>";
+echo "Mempunya kaki sebanyak : " . $momo->jumlah_kaki . "<br>";
 echo $momo->bisa_terbang . "<br>";
 echo "suaranya : " . $momo->bersuara() . "<br>";
 
-echo "<hr><hr>";
+echo "<hr>";
+
+
+$doggo = new Anjing;
+$doggo->jumlah_kaki = 4;
+echo "Doggo adalah Anjing <br>";
+echo "Mempunya kaki sebanyak : " . $doggo->jumlah_kaki . "<br>";
+echo $doggo->bisa_terbang . "<br>";
+echo "suaranya : " . $doggo->bersuara() . "<br>";
+
+echo "<hr>";
+
+
+$zya = new Elang;
+$zya->jumlah_kaki = 2;
+echo "Zya adalah Elang <br>";
+echo "Mempunya kaki sebanyak : " . $zya->jumlah_kaki . "<br>";
+echo $zya->bisa_terbang . "<br>";
+echo "suaranya : " . $zya->bersuara() . "<br>";
+
+echo "<hr>";
+
+
+$masha = new Angsa;
+$masha->jumlah_kaki = 2;
+echo "Masha adalah Angsa <br>";
+echo "Mempunya kaki sebanyak : " . $masha->jumlah_kaki . "<br>";
+echo $masha->bisa_terbang . "<br>";
+echo "suaranya : " . $masha->bersuara() . "<br>";
+
+echo "<hr><hr><hr>";
